b5bf80ef8310a5bba7a662063ea915306f58fde8
Adrian Chong <adrianchong518@gmail.com>
2023-05-24 22:21:28 +0800

    init zig project

---
+++ README.md
@@ -0,0 +1,4 @@
+# lox-zig
+
+A bytecode VM compiler / interpreter for the Lox language written in Zig from the book [Crafting Interpreters](https://craftinginterpreters.com/)
+
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "avrzim";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zls = {
+      url = "github:zigtools/zls";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.zig-overlay.follows = "zig-overlay";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, zig-overlay, zls, ... }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            zig-overlay.packages.${system}.master
+            zls.packages.${system}.default
+          ];
+        };
+      }
+    );
+}
